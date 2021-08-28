3840c71662e92928205bed6afee6969b554ed183
Kelsey Duru <kdassignments@gmail.com>
2021-08-28 15:07:02 -0700

    added confirmation prompts through JS.

--- script.js
+++ script.js
@@ -19,31 +19,44 @@ var upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "
 function generatePassword() {
   var length = prompt
     ("Pick a length for your password");
-// makes sure that user inputs the right characters
-      var length = userInput;
-      if (!userInput) {
-        alert("Input is needed");
-        return;
-      } else if (isNaN(length)) {
-        alert("Must input number value");
-        return;
-      } else if (length < 8 || length > 128) {
-        alert("Must be between 8 and 128");
-        return
-      } else if (length >= 8 && length <= 128) {
-        
-  // Assignment Code
-  var generateBtn = document.querySelector("#generate");
+  // makes sure that user inputs the right characters
+  var length = userInput;
+  if (!userInput) {
+    alert("Input is needed");
+    return;
+  } else if (isNaN(length)) {
+    alert("Must input number value");
+    return;
+  } else if (length < 8 || length > 128) {
+    alert("Password must be between 8 and 128 characters");
+    return
 
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+  //once user inputs correct values, the prompts will then begin to ask the user which characters they would want to be a part of their generated passcode
+  
+  } else if (length >= 8 && length <= 128) {
+    numChoice = window.confirm("Did you want the password to contain numbers?");
+    lowCaseChoice = window.confirm(
+      "Did you want the password to contain lowercase letters?"
+    );
+    upCaseChoice = window.confirm(
+      "Did you want the password to contain uppercase letters?"
+    );
+    specCharChoice = window.confirm(
+      "Did you want the password to contain special characters?"
+    );
 
-    passwordText.value = password;
+    // Assignment Code
+    var generateBtn = document.querySelector("#generate");
+
+    // Write password to the #password input
+    function writePassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+
+      passwordText.value = password;
 
 
 
 
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+      // Add event listener to generate button
+      generateBtn.addEventListener("click", writePassword);
