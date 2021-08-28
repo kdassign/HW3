84115f990ffbb7e67b2cb43fa92e0845fa512f7f
Kelsey Duru <kdassignments@gmail.com>
2021-08-27 23:24:00 -0700

    add choices and generate password function

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
+// prompts will show up when asking user whether they want the following characters to be 
+var numChoice;
+var specCharChoice;
+var upCaseChoice;
+var lowCaseChoice;
 
 // this is the number array for the password generator
-var numChar = [0,1,2,3,4,5,6,7,8,9];
+var numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // this is the array so that special characters can be included in generated passwords
 var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "-", "+"];
 // this is the array for lower case characters
-var lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // this is the array for upper case characters
-var upCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var length = prompt
+    ("Pick a length for your password");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // Assignment Code
+  var generateBtn = document.querySelector("#generate");
 
-}
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    passwordText.value = password;
+
+  }
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
